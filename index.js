9fc184ce13b87b113cc492bdb1dda1fc3723042d
maxwellkiplagat <maxwellkiplagat0@gmail.com>
2025-03-03 23:24:05 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someSample){
   // Write your code here!
+if (someSample<=400) {
+  return ( `This one is on me!`)  
+}else if(someSample>400 && someSample<2000){
+  return `That will be twenty bucks.`
+}else if(someSample>2000 && someSample<2500){
+  return `I will gladly take your thirty bucks.`
+}else if(someSample>2500){
+  return `No can do.`
+}
 }
 
-function ternaryCheckCity(){
+
+function ternaryCheckCity(someCheck){
   // Write your code here!
+  return someCheck==='NYC'? (someCheck = `Ok, sounds good.`):(someCheck =`No go.`)
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(myCharm){
   // Write your code here!
+  switch (myCharm) {
+    case 'generous':
+      return`Thank you so much.`
+      break;
+  
+    case 'not as generous':
+      return `Thank you.`
+      break;
+    case 'thanks for everything':
+      return `Bye.`
+      break
+  }
 }
